8fe6f24b7b4c3affb4800090adb531014b842263
Julian <julian.sz.orlowski@gmail.com>
2026-02-17 12:43:41 +0100

    feat: add ol parser, eof handling in ul

--- src/Markdown/Parser.hs
+++ src/Markdown/Parser.hs
@@ -1,34 +1,42 @@
-{-# LANGUAGE GADTs, LambdaCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Parser where
+
 import Control.Applicative (Alternative (..))
 import Control.Monad (void)
+import Data.Char (isDigit)
+
+data ListType = Ordered | Unordered
+  deriving (Show)
 
 data Block
   = CodeBlock (Maybe String) String
   | Heading Int String
-  | List [String]
+  | List ListType [String]
   | Paragraph [Inline]
-  deriving Show
+  deriving (Show)
 
 data Inline
   = Text String
   | Bold String
   | Italic String
   | Code String
+  | Link [Inline] String
   | Newline
-  deriving Show
+  deriving (Show)
 
 data MarkdownDoc where
   MarkdownDoc :: [Block] -> MarkdownDoc
-  deriving Show
+  deriving (Show)
 
-newtype Parser a = Parser {runParser :: String -> Maybe (a, String) }
+newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
 
 instance Functor Parser where
   fmap f p = Parser $ \input ->
     case runParser p input of
       Nothing -> Nothing
-      Just (x,rest) -> Just (f x,rest)
+      Just (x, rest) -> Just (f x, rest)
 
 instance Applicative Parser where
   pure x = Parser $ \input -> Just (x, input)
@@ -36,9 +44,9 @@ instance Applicative Parser where
   pf <*> px = Parser $ \input ->
     case runParser pf input of
       Nothing -> Nothing
-      Just (f,rest) -> case runParser px rest of
+      Just (f, rest) -> case runParser px rest of
         Nothing -> Nothing
-        Just (x,rest) -> Just (f x, rest)
+        Just (x, rest) -> Just (f x, rest)
 
 instance Alternative Parser where
   empty = Parser $ const Nothing
@@ -56,23 +64,24 @@ instance Monad Parser where
 
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy f = Parser $ \case
-    [] -> Nothing
-    (x:xs) -> if f x then Just (x,xs) else Nothing
+  [] -> Nothing
+  (x : xs) -> if f x then Just (x, xs) else Nothing
 
 char :: Char -> Parser Char
-char c = satisfy (==c)
+char c = satisfy (== c)
 
 anyChar :: Parser Char
 anyChar = Parser $ \case
   [] -> Nothing
-  (x:xs) -> Just (x,xs)
+  (x : xs) -> Just (x, xs)
 
 string :: String -> Parser String
 string [] = pure []
-string (c:cs) =
-  char c >>
-  string cs >>= \rest ->
-  return (c:rest)
+string (c : cs) =
+  char c
+    >> string cs
+    >>= \rest ->
+      return (c : rest)
 
 manyTill :: Parser a -> Parser end -> Parser [a]
 manyTill p end = (end >> pure []) <|> (:) <$> p <*> manyTill p end
@@ -81,12 +90,12 @@ someTill :: Parser a -> Parser end -> Parser [a]
 someTill p end = do
   f <- p
   rest <- manyTill p end
-  pure $ f:rest
+  pure $ f : rest
 
 eof :: Parser ()
 eof = Parser $ \case
-    [] -> Just ((),[])
-    _ -> Nothing
+  [] -> Just ((), [])
+  _ -> Nothing
 
 skipWhitespace :: Parser ()
 skipWhitespace = void $ many (char '\n' <|> char ' ' <|> char '\t')
@@ -109,6 +118,18 @@ parseCode = do
   text <- manyTill anyChar (char '`')
   pure $ Code text
 
+parseLink = do
+  char '['
+  text <- someTill anyChar (char ']')
+  let Just (inlines, _) = runParser parseInlines text
+  char '('
+  link <- someTill anyChar (char ')')
+  pure $ Link inlines link
+
+-- char '('
+-- link <- someTill anyChar (char ')')
+-- pure $ Link text link
+--
 parseText = do
   text <- some (satisfy (`notElem` ['*', '`', '\n']))
   pure $ Text text
@@ -119,8 +140,15 @@ parseNewline = do
   char '\n'
   pure Newline
 
-parseInline = parseBold <|> parseItalic <|> parseCode <|> parseText <|> parseNewline
+parseInline =
+  parseBold
+    <|> parseItalic
+    <|> parseCode
+    <|> parseLink
+    <|> parseText
+    <|> parseNewline
 
+parseInlines :: Parser [Inline]
 parseInlines = some parseInline
 
 -- Blocks
@@ -136,35 +164,44 @@ parseHeading = do
 parseCodeBlock :: Parser Block
 parseCodeBlock = do
   string "```"
-  lang <- many (satisfy (/='\n'))
+  lang <- many (satisfy (/= '\n'))
   char '\n'
   code <- manyTill anyChar (string "```")
   case lang of
     [] -> pure $ CodeBlock Nothing code
-    _ ->  pure $ CodeBlock (Just lang) code
+    _ -> pure $ CodeBlock (Just lang) code
 
 parseList :: Parser Block
 parseList = do
-  l <- parseListItem
-  rest <- many parseListItem
-  pure $ List $ l:rest
+  items <- some parseListItem
+  pure $ List Unordered items
   where
     parseListItem = do
-      char '-'
-      -- <|> char '*'
-      char ' '
-      manyTill anyChar (char '\n')
+      string "- "
+      manyTill anyChar (void (char '\n') <|> eof)
+
+parseOrderedList :: Parser Block
+parseOrderedList = do
+  items <- some parseListItem
+  pure $ List Ordered items
+  where
+    parseListItem = do
+      satisfy isDigit
+      string ". "
+      manyTill anyChar (void (char '\n') <|> eof)
 
 parseParagraph :: Parser Block
 parseParagraph = Paragraph <$> someTill parseInline (void (string "\n\n") <|> eof)
 
 parseBlock :: Parser Block
-parseBlock = (
-  parseHeading
-  <|> parseCodeBlock
-  <|> parseList
-  <|> parseParagraph
-  ) <* (skipWhitespace <|> eof)
+parseBlock =
+  ( parseHeading
+      <|> parseCodeBlock
+      <|> parseList
+      <|> parseOrderedList
+      <|> parseParagraph
+  )
+    <* (skipWhitespace <|> eof)
 
 parseBlocks :: Parser [Block]
 parseBlocks = some parseBlock
